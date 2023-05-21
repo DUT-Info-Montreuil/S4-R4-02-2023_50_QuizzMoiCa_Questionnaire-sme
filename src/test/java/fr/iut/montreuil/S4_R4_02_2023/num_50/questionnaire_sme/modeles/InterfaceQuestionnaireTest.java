1122015d43c61bd4ffb39ad4a054af029e3f0a5e
Philippe20033 <philippesantos2003@gmail.com>
2023-05-21 14:38:00 +0200

    Delete default Unused packages and add of Junit dependences

---
+++ src/test/java/fr/iut/montreuil/S4_R4_02_2023/num_50/questionnaire_sme/modeles/InterfaceQuestionnaireTest.java
@@ -0,0 +1,13 @@
+package fr.iut.montreuil.S4_R4_02_2023.num_50.questionnaire_sme.modeles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class InterfaceQuestionnaireTest {
+
+    public void testchargerFileQuestionnaire(){
+
+    }
+
+}
