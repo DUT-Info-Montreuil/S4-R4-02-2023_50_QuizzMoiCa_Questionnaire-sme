4686b22b4bb8c8577af5f9337ec55ac18b878b6d
Philippe20033 <philippesantos2003@gmail.com>
2023-05-21 15:45:19 +0200

    Rebase Mockito package and tests with more logic

---
+++ src/test/java/fr/iut/montreuil/S4_R4_02_2023/num_50/questionnaire_sme/mocks/ajouterQuestionnaireIncorectMock.java
@@ -0,0 +1,15 @@
+package fr.iut.montreuil.S4_R4_02_2023.num_50.questionnaire_sme.mocks;
+
+import fr.iut.montreuil.S4_R4_02_2023.num_50.questionnaire_sme.entites.bo.QuestionBO;
+import fr.iut.montreuil.S4_R4_02_2023.num_50.questionnaire_sme.entites.bo.QuestionnaireBO;
+import fr.iut.montreuil.S4_R4_02_2023.num_50.questionnaire_sme.modeles.InterfaceQuestionnaire;
+import fr.iut.montreuil.S4_R4_02_2023.num_50.questionnaire_sme.utilies.exeptions.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+public class ajouterQuestionnaireIncorectMock {
+    @Override
+    public QuestionnaireBO chargerFileQuestionnaire(URI path) throws FileNoteFoundExeption, ErrorWhileLoadingException, QuestionnaireEmptyException, DoubleQuestionException, WrongFileTypeException {
+        return null;
+    }
+}
